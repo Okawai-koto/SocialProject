da371b31c81d2e29076765f294f9cff38f1afe0e
FIRAT ARSLAN <f.lion.trr@gmail.com>
2023-05-16 21:18:45 +0300

    son yapılan değişikliklerin yüklenmesi

--- signupService/main.go
+++ signupService/main.go
@@ -1,29 +1,24 @@
 package main
 
 import (
+	"signupservice/routes"
+
 	"github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
+// user represents data about a record album.
 type user struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
-// albums slice to seed record album data.
-var users = []user{
-	{Email: "1", Password: "Blue Train"},
-	{Email: "2", Password: "Jeru"},
-}
-
-var exampleuser = user{Email: "2", Password: "pasuwado"}
-
 func main() {
 
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
+	router.GET("/users", routes.GetUsers)
+	router.POST("/users", routes.PostUsers)
+
+	router.GET("/users/:email", routes.GetUserByEmail)
 
 	router.Run("localhost:8080")
 
